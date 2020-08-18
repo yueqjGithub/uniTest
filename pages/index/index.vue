4317541f3b41c1b38e4c19ad424f8bfb31a0a8b8
yqj <410281036@qq.com>
2020-08-18 14:26:21 +0800

    static下增加icon示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="container">
 		<view class="intro text-primary">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
 		<text class="intro">详见：{{isLogin}}</text>
-		<view class='iconfont icon-xuanze'></view>
+		<view class='iconfont icon-xuanze text-primary'></view>
 		<uni-link :href="href" :text="href"></uni-link>
 		<button type="default" @click="setLogin('试试')">变更</button>
 	</view>
