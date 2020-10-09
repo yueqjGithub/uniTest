b5b128300c542886e93f6cca1f042f87f45316bb
yueqjGit <410281036@qq.com>
2020-10-09 20:09:39 +0800

    创建electric分包

---
+++ electric/electricIndex/electricIndex.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="page bg-grey">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.page{
+	width: 100vw;
+	height: 100vh;
+}
+</style>
