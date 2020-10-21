7755797ddbe6743767a85f551e41115cac3485a8
yueqj <410281036@qq.com>
2020-10-21 23:22:53 +0800

    add

---
+++ oil/card/card.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="page bg-grey">
+		<view class="head-bg"></view>
+		<view class="cont-item pa-md flex-column flex-jst-center flex-ali-center">
+			<view class="address-icon flex-row flex-jst-center flex-ali-center">
+				<u-icon name="ditujiayouzhan" size="90" class="text-primary" custom-prefix="iconfont"></u-icon>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import { mapState } from 'vuex'
+	import urls from '@/service/urls.js'
+	export default {
+		name: 'card',
+		data() {
+			return {
+			}
+		},
+		watch: {
+			lang: {
+				immediate: true,
+				handler: function (val) {
+					if (val !== 'zh-CN') {
+						uni.setNavigationBarTitle({
+							title: this._i18n.messages[val].oilCard.pageName
+						})
+					}
+				}
+			}
+		},
+		computed: {
+			...mapState(['lang']),
+			langFlex () {
+				return this.lang === 'zh-CN' ? 'flex-row' : 'flex-row-reverse'
+			},
+			rightClass() {
+				return this.lang === 'zh-CN' ? '' : 'my-text-right'
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+.page{
+		width: 100%;
+		height: 100vh;
+		padding-top: 17px;
+		.head-bg {
+			position: absolute;
+			z-index: 1;
+			top: 0;
+			left: 0;
+			box-sizing: border-box;
+			width: 100%;
+			height: 256.8rpx;
+			border-bottom-left-radius: 50vw 5vh;
+			border-bottom-right-radius: 50vw 5vh;
+			padding-top: 20rpx;
+			margin-bottom: 30rpx;
+			background: linear-gradient(0deg, #19C882, #23AF8C);
+		}
+		.cont-item{
+			position: relative;
+			z-index: 2;
+			width: 90%;
+			margin: 50rpx auto;
+			background: #FFFFFF;
+			box-shadow: 0px 10px 24.3rpx 0px rgba(170, 170, 170, 0.1);
+			border-radius: 20.83rpx;
+			.address-icon {
+				border: 1px solid rgba(207, 207, 207, 1);
+				border-radius: 13.88rpx;
+				width: 138.88rpx;
+				height: 138.88rpx;
+			}
+		}
+	}
+</style>
