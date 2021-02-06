006a1ff569c070582a287b679b304fe0fa997e23
yueqjGit <410281036@qq.com>
2021-02-06 15:54:11 +0800

    add

--- pullOtherPage/withdraw/withdraw.vue
+++ pullOtherPage/withdraw/withdraw.vue
@@ -10,8 +10,8 @@
 				<text class="text-grey-1 text-12">{{$t('pullPage.balance')}}</text>
 			</view>
 			<view class="full-width pa-md border-box"></view>
-			<view class="amount full-width flex-jst-start flex-ali-center" :class="langFlex">
-				<u-input v-model="amount" :class="inputClass" :custom-style="{color: '#aaaaaa', fontSize: '16px'}" :clearable="false"
+			<view class="amount full-width flex-jst-center flex-ali-center" :class="langFlex">
+				<u-input v-model="amount" :custom-style="{fontSize: '16px', textAlign: 'center'}" :clearable="false"
 				 placeholder-style="font-family:'cusFont','yahei';color: #aaaaaa;padding: 0 5px;" :placeholder="$t('pullPage.amountTips')" type="number"></u-input>
 			</view>
 			<view class="full-width pa-md flex-row flex-jst-center flex-ali-center border-box">
